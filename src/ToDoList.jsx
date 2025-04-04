85d5378ff8445cc15cb582730c948bd79e81c615
mariomartz0 <mariomartz2018@gmail.com>
2025-04-03 20:59:21 -0400

    Turn ToDoItem into a separate component

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import CommentIcon from "@mui/icons-material/Comment";
+import ToDoItem from "./ToDoItem";
 
 const initialToDos = [
   { id: 1, text: "Feed the dog.", completed: false },
@@ -20,34 +14,9 @@ export default function ToDoList() {
 
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      {toDos.map((toDo) => {
-        const labelId = `checkbox-list-label-${toDo.id}`;
-
-        return (
-          <ListItem
-            key={toDo.id}
-            secondaryAction={
-              <IconButton edge="end" aria-label="comments">
-                <CommentIcon />
-              </IconButton>
-            }
-            disablePadding
-          >
-            <ListItemButton role={undefined} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={toDo.completed}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ "aria-labelledby": labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={toDo.text} />
-            </ListItemButton>
-          </ListItem>
-        );
-      })}
+      {toDos.map((toDo) => (
+        <ToDoItem toDo={toDo} key={toDo.id} />
+      ))}
     </List>
   );
 }
